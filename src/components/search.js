2e1636b9b5398baa73571ee5ea17d0921cf25419
Christian Medinilla <cmedinilla@intersysconsulting.com>
2017-11-23 16:49:37 -0600

    Upload app

---
+++ src/components/search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="searcho">
+        <p className="search-intro">
+          kakas verdes doradas con calzones.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Search;
