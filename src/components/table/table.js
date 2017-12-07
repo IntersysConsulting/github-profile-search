99ce1b59e00edc711a45ee7f30bbfdaca8905cc7
Christian Medinilla <cmedinilla@intersysconsulting.com>
2017-12-07 17:40:11 -0600

    Add table configuration

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -2,13 +2,50 @@ import React, { Component } from 'react';
 import HotTable from 'react-handsontable';
 
 class Table extends Component {
+    constructor(props) {
+        super(props);
+        const columnsConf = [
+            { 
+              type: 'text',
+              readOnly: true
+            },
+            {
+              type: 'text',
+              readOnly: true
+            },
+            {
+              type: 'text',
+              readOnly: true
+            },
+            {
+              type: 'text',
+              readOnly: true
+            },
+            {
+              type: 'text',
+              readOnly: true
+            },
+            {
+              type: 'text',
+              readOnly: true
+            }
+          ] 
+        this.state = {
+            value: 'Scala',
+            columnHeaders: ['Profile','Location','Followers','GitHub User', 'Github Profile', 'Repositories'],
+            columns: columnsConf,
+            followers: props.followers || null
+        };
+    };
+
   render() {
     return (
         <div>
             <HotTable 
-                data={[['some', 'test'], ['handsontable', 'data']]} 
-                contextMenu={true} 
-                colHeaders={true} 
+                data={[['some', 'test','yes'], ['handsontable', 'data']]} 
+                contextMenu={false} 
+                colHeaders = {this.state.columnHeaders}
+                columns = {this.state.columns}
             />
         </div>
     );
