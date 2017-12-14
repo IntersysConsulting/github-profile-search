37dc3244672bcfedb57409ed48c1b09e8b80b954
lordzero0000 <LordZero0000@gmail.com>
2017-12-14 12:59:41 -0600

    Add fetch
    * Merge conflicts
    
    * Adding fetch data from github
    
    * Remove console logs

--- src/utils/github.js
+++ src/utils/github.js
@@ -1,26 +1,10 @@
 const 
-  githubUrl = `https://api.github.com`
-  searchRequest = (typeSearch, query) => {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest()
-      xhr.withCredentials = true
-
-      xhr.addEventListener("readystatechange", () => {
-        if (this.readyState === 4) {
-          return resolve(this.responseText)
-          console.log(this.responseText)
-        } else {
-          return reject(null)
-        }
-      })
-
-      xhr.open("GET", `${githubUrl}/${typeSearch}?${query}`)
-      xhr.send()
-    })
-}
+  githubUrl = `https://api.github.com`,
+  fetchData = (typeSearch, query) => fetch(`${githubUrl}/${typeSearch}?${query}`, { method: 'GET', headers: { "Authorization": "token 7515253eb4e8956f5e6ec183ed8d3ccb80ce7726"} })
 
 module.exports = {
-  searchByUsername: name => searchRequest(`search/users`, `q=${name}`),
-  searchByUserByLanguage: (name, language) => searchRequest(`search/users`, `q=${name}+language:${language}`),
-  searchByUserByLocation: (name, location) => searchRequest(`search/users`, `q=${name}+location:${location}`)
+  searchByUsername: name => fetchData(`search/users`, `q=${name}`),
+  searchByUserByLanguage: (name, language) => fetchData(`search/users`, `q=${name}+language:${language}`),
+  searchByUserByLocation: (name, location) => fetchData(`search/users`, `q=${name}+location:${location}`),
+  searchByLanguageAndLocation: (language, location) => fetchData(`search/users`, `q=language:${language}+location:${location}`)
 }
